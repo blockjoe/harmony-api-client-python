2cdf1dd1c36ae3e079a570fd56deb49e9fb6698f
blockjoe <69400937+blockjoe@users.noreply.github.com>
2021-01-15 21:06:07 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="harmony-api-client-python",
+    author="blockjoe, gretha",
+    author_email="joehabel46@gmail.com",
+    description="A python client for interacting with Harmony endpoints.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blockjoe/harmony-api-client-python",
+    packages=setuptools.find_packages(),
+    license="Apache License 2.0",
+    python_requires=">=3.7",
+    install_requires=[
+        'pydantic',
+        'requests',
+        'rosetta-api-client-python @ git+https://github.com/blockjoe/rosetta-api-client-python#egg=rosetta-api-client-python'
+    ],
+    extras_require = {
+        'dev' : ['datamodel-code-generator', 'sphinx', 'sphinx-rtd-theme']
+    }
+)
